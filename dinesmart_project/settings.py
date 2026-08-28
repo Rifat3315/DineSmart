e12243f0ce5c8f312397cf6b0b5232af9bf536bb
Rifat3315 <mdrifathossain3315@gmail.com>
2026-08-29 00:51:38 +0600

    Fix static files for Render

--- dinesmart_project/settings.py
+++ dinesmart_project/settings.py
@@ -153,9 +153,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'frontend' / 'static']
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'frontend' / 'static'
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
